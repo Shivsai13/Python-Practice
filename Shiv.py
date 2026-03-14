1b4ee5e5d868c7dc2f12edaf52dde1e304cd18c5
Shivsai13 <karna.ncu@gmail.com>
2026-03-14 10:20:54 -0400

    Add files via upload

---
+++ Shiv.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print("Welcome to the world ofprogramming!")
+
+#Print a Number
+print(4)
+
+#Next line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#end argument of Print Statement
+print("welcome to ", end= "*")
