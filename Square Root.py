e7dca8b8e4132ded439ddbd88e193095fdf86ca2
Shivsai13 <karna.ncu@gmail.com>
2026-03-27 20:09:25 -0400

    Add files via upload

---
+++ Square Root.py
@@ -0,0 +1,7 @@
+num = float(input('Enter a number: '))
+
+# Calculate the square root using the exponentiation operator
+num_sqrt = num ** 0.5
+
+# Print the result
+print(f'The square root of {num} is {num_sqrt}')
