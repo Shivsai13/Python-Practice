2377db42d4fe0f62f01ef3de7b6d6c8fa981b333
Shivsai13 <karna.ncu@gmail.com>
2026-03-28 10:12:54 -0400

    Add files via upload

---
+++ Check.py
@@ -0,0 +1,14 @@
+num = 3
+if num > 0:
+    print (num, "is a positive number.")
+
+num = -1
+if num > 0:
+    print (num, "is a positive number.")
+
+i = 4
+
+if (i < 15):
+    print(i, "is smaller than 15.")
+if (i > 15):
+    print(i, "is greater than 15.")
